0cc4ed94d39b7150047b149e0a77fb3a6e0c809e
vinhduc5984 <phungvinhduc3@gmail.com>
2021-05-12 19:04:54 +0700

    api ken

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,5 +9,12 @@ router.use('/auth', customer);
 
 router.use('/user', user);
 router.use('/company', company);
+router.get('/ken', (req, res, next) => {
+  res.json({
+    name: 'Lam Hoang An',
+    age: '20',
+    hoby: '123',
+  });
+});
 
 module.exports = router;

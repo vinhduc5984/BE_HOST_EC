369e8bced427ee35736addd03be95f8542c2a89e
vinhduc5984 <phungvinhduc3@gmail.com>
2021-06-18 10:51:32 +0700

    [New]: Statistic Bill for Admin

--- src/controllers/statistic.Constroler.js
+++ src/controllers/statistic.Constroler.js
@@ -14,4 +14,66 @@ const revenuestatistic = async (req, res, next) => {
   return controller.sendSuccess(res, {}, ResService.statusCode, ResService.msg);
 };
 
-module.exports = { revenuestatistic };
+const invoiceStatisticChoXacNhan = async (req, res, next) => {
+  const tokenID = req.value.body.token.data;
+  const ResService = await StatisticService.InvoiceStatisticChoXacNhan(tokenID);
+  if (ResService.statusCode === 200) {
+    return controller.sendSuccess(
+      res,
+      ResService.data,
+      ResService.statusCode,
+      ResService.msg,
+    );
+  }
+  return controller.sendSuccess(res, {}, ResService.statusCode, ResService.msg);
+};
+
+const invoiceStatisticDangGiao = async (req, res, next) => {
+  const tokenID = req.value.body.token.data;
+  const ResService = await StatisticService.InvoiceStatisticDangGiao(tokenID);
+  if (ResService.statusCode === 200) {
+    return controller.sendSuccess(
+      res,
+      ResService.data,
+      ResService.statusCode,
+      ResService.msg,
+    );
+  }
+  return controller.sendSuccess(res, {}, ResService.statusCode, ResService.msg);
+};
+
+const invoiceStatisticDaGiao = async (req, res, next) => {
+  const tokenID = req.value.body.token.data;
+  const ResService = await StatisticService.InvoiceStatisticDaGiao(tokenID);
+  if (ResService.statusCode === 200) {
+    return controller.sendSuccess(
+      res,
+      ResService.data,
+      ResService.statusCode,
+      ResService.msg,
+    );
+  }
+  return controller.sendSuccess(res, {}, ResService.statusCode, ResService.msg);
+};
+
+const invoiceStatisticDaHuy = async (req, res, next) => {
+  const tokenID = req.value.body.token.data;
+  const ResService = await StatisticService.InvoiceStatisticDaHuy(tokenID);
+  if (ResService.statusCode === 200) {
+    return controller.sendSuccess(
+      res,
+      ResService.data,
+      ResService.statusCode,
+      ResService.msg,
+    );
+  }
+  return controller.sendSuccess(res, {}, ResService.statusCode, ResService.msg);
+};
+
+module.exports = {
+  revenuestatistic,
+  invoiceStatisticChoXacNhan,
+  invoiceStatisticDangGiao,
+  invoiceStatisticDaGiao,
+  invoiceStatisticDaHuy,
+};
